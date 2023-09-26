2ea004cb176aed83e7c43846d9bc3d54b6bb47d8
Ahnder <skguss1@gmail.com>
2023-09-26 20:18:24 +0900

    create_user read csvfile test

--- simple_login.py
+++ simple_login.py
@@ -3,22 +3,29 @@ import csv
 
 # 새로운 유저 정보를 작성하는 함수
 def create_user():
-    # 사용자에게 ID와 비밀번호를 입력받는다
-    user_id = input("ID: ")
-    user_password = input("PASSWORD: ")
+    # # 사용자에게 ID와 비밀번호를 입력받는다
+    # user_id = input("ID: ")
+    # user_password = input("PASSWORD: ")
 
-    data = (user_id, user_password)
+    # data = (user_id, user_password)
     
-    # csv파일 write 하기 위한 리스트
-    data_list = list()
-    data_list.append(data)
+    # # csv파일 write 하기 위한 리스트
+    # data_list = list()
+    # data_list.append(data)
 
     # 사용 할 csv_filename
     csv_filename = "userinfo.csv"
+
+    # csv 파일 읽기
+    with open(csv_filename, 'r') as f:
+        reader = csv.reader(f)
+        read_user_list = list(reader)
     # csv 파일 쓰기
-    with open(csv_filename, 'w') as f:
-        writer = csv.writer(f)
-        writer.writerows(data_list)
+    # with open(csv_filename, 'w') as f:
+    #     writer = csv.writer(f)
+    #     writer.writerows(data_list)
+
+    return read_user_list
 
 
 # 로그인 프로그램 메인메뉴 함수
